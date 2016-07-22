8d7983da8b82bae367304095e8193e443e98dbe8
Timothy J. Aveni <timothy@tacosareawesome.com>
2016-07-22 12:31:25 -0400

    Remove logging

--- selection.js
+++ selection.js
@@ -1,9 +1,7 @@
 var subjects = ['ACCT', 'AE', 'AS', 'APPH', 'ASE', 'ARBC', 'ARCH', 'BIOL', 'BMEJ', 'BMED', 'BMEM', 'BCP', 'BC', 'CETL', 'CHBE', 'CHEM', 'CHIN', 'CP', 'CEE', 'COA', 'COE', 'COS', 'CX', 'CSE', 'CS', 'COOP', 'UCGA', 'EAS', 'ECON', 'ECEP', 'ECE', 'ENGL', 'FS', 'FREN', 'GT', 'GTL', 'GRMN', 'HP', 'HS', 'HIN', 'HIST', 'HTS', 'ISYE', 'ID', 'IPCO', 'IPIN', 'IPFS', 'IPSA', 'INTA', 'IL', 'INTN', 'IMBA', 'IAC', 'JAPN', 'KOR', 'LATN', 'LS', 'LING', 'LCC', 'LMC', 'MGT', 'MOT', 'MLDR', 'MSE', 'MATH', 'ME', 'MP', 'MSL', 'ML', 'MUSI', 'NS', 'NRE', 'PERS', 'PHIL', 'PHYS', 'POL', 'PTFE', 'DOPP', 'PSY', 'PSYC', 'PUBP', 'PUBJ', 'RUSS', 'SCI', 'SOC', 'SPAN'];
 var subjectRegex = new RegExp('(' + subjects.join('|') + ') ?([0-9]{4})', 'i');
-console.log(subjectRegex);
 
 document.addEventListener('selectionchange', function () {
-	console.log(getCourse(window.getSelection().toString()))
 	chrome.runtime.sendMessage({
 		"course": getCourse(window.getSelection().toString())
 	});
