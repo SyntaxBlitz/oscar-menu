4225fd1353536277673e2e24df8e5402ad334208
Timothy J. Aveni <timothy@tacosareawesome.com>
2016-07-22 12:29:00 -0400

    Replace Unicode space with ' ', revert last commit

--- selection.js
+++ selection.js
@@ -1,5 +1,5 @@
 var subjects = ['ACCT', 'AE', 'AS', 'APPH', 'ASE', 'ARBC', 'ARCH', 'BIOL', 'BMEJ', 'BMED', 'BMEM', 'BCP', 'BC', 'CETL', 'CHBE', 'CHEM', 'CHIN', 'CP', 'CEE', 'COA', 'COE', 'COS', 'CX', 'CSE', 'CS', 'COOP', 'UCGA', 'EAS', 'ECON', 'ECEP', 'ECE', 'ENGL', 'FS', 'FREN', 'GT', 'GTL', 'GRMN', 'HP', 'HS', 'HIN', 'HIST', 'HTS', 'ISYE', 'ID', 'IPCO', 'IPIN', 'IPFS', 'IPSA', 'INTA', 'IL', 'INTN', 'IMBA', 'IAC', 'JAPN', 'KOR', 'LATN', 'LS', 'LING', 'LCC', 'LMC', 'MGT', 'MOT', 'MLDR', 'MSE', 'MATH', 'ME', 'MP', 'MSL', 'ML', 'MUSI', 'NS', 'NRE', 'PERS', 'PHIL', 'PHYS', 'POL', 'PTFE', 'DOPP', 'PSY', 'PSYC', 'PUBP', 'PUBJ', 'RUSS', 'SCI', 'SOC', 'SPAN'];
-var subjectRegex = new RegExp('(' + subjects.join('|') + ')\s?([0-9]{4})', 'i');
+var subjectRegex = new RegExp('(' + subjects.join('|') + ') ?([0-9]{4})', 'i');
 console.log(subjectRegex);
 
 document.addEventListener('selectionchange', function () {
@@ -10,6 +10,7 @@ document.addEventListener('selectionchange', function () {
 });
 
 var getCourse = function (text) {
+	text = text.replace(/[ \u00A0\u1680\u180e\u2000-\u2009\u200a\u200b\u202f\u205f\u3000]/g, ' ');
 	var matches = text.match(subjectRegex);
 	var subject = null;
 	var courseId = null;
